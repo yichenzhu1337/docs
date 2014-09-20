8e8770e6519477ca88d5ce2a882469813ccbff72
yichen <yichenzhu1337@gmail.com>
2014-09-20 02:21:00 -0400

    twilio

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -19,5 +19,5 @@ class HomeController extends BaseController {
 	{
 		return View::make('hello');
 	}
-
+	
 }
--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,19 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/sms', function() 
+{
+	//require('vendor/twilio/sdk/Services'); //find
+	 
+	$account_sid = 'ACaf602a3c1207efe0343de5c9130a3340'; 
+	$auth_token = 'b20ac9f89c3a16bc8699274e06ea5c8a'; 
+	$client = new Services_Twilio($account_sid, $auth_token); 
+	 
+	$client->account->messages->create(array(  
+		'From' => "+12898073591",    
+		'To' => "4168561988",
+		'Body' => "This is form the webserver"
+	));
+
+});
